29e6981f3f47c9ce8c38723abdb8aff79e25fac1
Dmitri K <69704069+DtPeach3707@users.noreply.github.com>
2021-09-14 07:11:41 -0400

    Cleaned Up and Commented

--- background.js
+++ background.js
@@ -1,18 +1,18 @@
 // background.js
-// Detects tab change and executes improve.js if the url matches
+// Detects tab change and executes improve function if the url matches
 
-var shown = false;
+var shown = false; // Variables
 var shownb = false;
 
 
 function improve(type){
-   switch(type){
+   switch(type){ // Uses switch because more types will be added in the future for different class tabs
 	case 0:
 
-            var needrop = document.getElementById('facebox').getElementsByClassName('popup')[0]; 
+            var needrop = document.getElementById('facebox').getElementsByClassName('popup')[0]; // Element exists even when not actually visible
             var link = document.createElement('a');
             link.appendChild(document.createTextNode('Click Here to go to the Dropbox'));
-            link.href = "https://portals.veracross.com/webb/student/submit-assignments";
+            link.href = "https://portals.veracross.com/webb/student/submit-assignments"; // Changing attributes
             link.target = "_blank";
             link.title = "Dropbox Link";
             var li = document.createElement("div");
@@ -22,10 +22,10 @@ function improve(type){
             li.appendChild(document.createElement('br')); // Line breaks (to make it look nicer)
             li.appendChild(document.createElement('br'));
             li.classList.add("body"); // If not added than background would be behind the main page text
-            needrop.appendChild(li);
+            needrop.appendChild(li); // Adding to popup
 
 	default: // no popup (or no popup tab made)
-	    var valid = false;
+	    var valid = false; // Put this in because a default case will most likely be needed in the future
     }
 
 }
@@ -33,24 +33,22 @@ function improve(type){
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     let curtaburl = tab.url;
-    console.log(curtaburl);
     if (tab.url.match(/classes.veracross.com/)) {
-	if (!(shown)) {
-	    chrome.scripting.executeScript({target: {tabId: tabId}, func: improve, args: [0]});
+	if (!(shown)) { // Don't want to have 4 links in the popup; only one
+	    chrome.scripting.executeScript({target: {tabId: tabId}, func: improve, args: [0]}); // Executes improve function
             shown = true;
 	    shownb = false;
         }
     }
     else if (tab.url.match(/portals.veracross.com/) && tab.url.match(/student/)) {
-	if (!(shownb)) {
-	    console.log("Here now");
-	    chrome.scripting.executeScript({target: {tabId: tabId}, func: improve, args: [1]});
+	if (!(shownb)) { 
+	    chrome.scripting.executeScript({target: {tabId: tabId}, func: improve, args: [1]}); // No compatibility for this in improve yet, but working on it!
             shownb = true;
             shown = false;
 	}
     }
     else {
-	shown = false;
+	shown = false; // Nothing is being shown
 	shownb = false;
     }
 
