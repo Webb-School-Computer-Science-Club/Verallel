c95e13da23bd303ba77fe7d7163b4c7f173ccd6a
Dmitri K <69704069+DtPeach3707@users.noreply.github.com>
2021-09-14 07:13:31 -0400

    Comments

--- popup.js
+++ popup.js
@@ -1,24 +1,24 @@
 // Initialize button with user's preferred mode
 
-var lightmode = true;
+var lightmode = true; // Defining variables and queryselector so popup.css variables can be changed
 var r = document.querySelector(':root');
 
 document.getElementById("changeColor").addEventListener("click", changeMode);
 
 
-function darkMode(){
+function darkMode(){ // Changes popup.css variables
     r.style.setProperty('--background-color', '#000000');
     r.style.setProperty('--border-color', '#ffffff');
 }
 
 
-function lightMode(){
+function lightMode(){ // Changes popup.css variables
     r.style.setProperty('--background-color', '#ffffff');
     r.style.setProperty('--border-color', '#000000');
 }
 
 
-function changeMode(){
+function changeMode(){ // Main function that popup.html calls
     if (lightmode == true){
 	changeColor.innerHTML = "Press button for light mode";
 	lightmode = false;
