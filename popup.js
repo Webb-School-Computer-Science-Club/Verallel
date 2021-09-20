0dc1748f3822f52beb0be5575194114333b42ce3
Dmitri K <69704069+DtPeach3707@users.noreply.github.com>
2021-09-20 16:23:40 -0400

    Now places assignments due today and tmrw first
    This code is probably very inefficient, but it works for its intended purpose (which is, to display assignments due the current day and the day after that current date first and second respectivley)

--- popup.js
+++ popup.js
@@ -1,83 +1,155 @@
 // popup.js
 // Does notification button for assignments
 // One minor bug: \" in assignments doesn't get replaced by "
-// Next update will be to sort by date in comparison to today's date
+// May or may not need to sort by year, but that will probably be next update for getAssignments()
 
 document.getElementById("notif").addEventListener("click", reqNotify);
 var assign = false;
+const monthdict = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12}; //3-letter month to number conversion
 
-async function getAssignments(){ // Fetches data and adds data to popup
+async function getAssignments(){
     const respNew = await fetch('https://portals.veracross.com/webb/student/student/upcoming-assignments');
     const txt = await respNew.text();
     const stuId = txt.split(';')[15].split('&')[txt.split(';')[15].split('&').length - 2].split('=')[txt.split(';')[15].split('&')[txt.split(';')[15].split('&').length - 2].split('=').length - 1];
     const yr = txt.split(';')[16].split('"')[0].split('=')[1];
     const fetchUrl = 'https://portals-embed.veracross.com/webb/parent/planner?p=' + stuId + '&school_year=' + yr;
-    const response = await fetch(fetchUrl); //Fetching relevant data
-    const fg = await response.text(); //Unfortunatley retrieving as text was the only option that worked (json and blob didn't work), so have to split up text
+    const response = await fetch(fetchUrl);
+    const fg = await response.text();
     let today = new Date();
-    console.log(today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate());
-    const fh = ((fg.split(';')[15]).split('},{')); //Getting to the actual script with all the assignments
-    var assignments = []; //Assignment list and classRows dictionary object
+    let tommorow = new Date(today);
+    tommorow.setDate(tommorow.getDate() + 1);
+    const fh = ((fg.split(';')[15]).split('},{'));
+    var assignments = [];
     var classRows = {};
+    var assignDate = {};
     var isOn = false;
-    for (let i = 0; i < fh.length; i++){ //Iterates through all the lists of fh
-	    if (fh[i].split(',')[2].split(':')[0] == '"class_id"'){ // For associating rows with classes. This works because class id #s are listed before anything else
-	        classRows[fh[i].split(',')[0].split(':')[1]] = fh[i].split(',')[3].split(':')[1]; //Row-class associations
+    for (let i = 0; i < fh.length; i++){
+        var isToday = false;
+        var isTmrw = false;
+	    if (fh[i].split(',')[2].split(':')[0] == '"class_id"'){
+	        classRows[fh[i].split(',')[0].split(':')[1]] = fh[i].split(',')[3].split(':')[1];
 	    }
-        if ((fh[i].split(',')[3]).split(':')[1] == '"assignment-upcoming"'){ // An upcoming assignment
-            var assignStr = ''; //String variable with somewhat decently-formatted assignment
-            let commaMuch = fh[i].split(',').length - 11 // For dealing with assignments with commas in their description
-            if (commaMuch <= 0){ // No commas in assignment description
-                assignStr = (fh[i].split(',')[7]).split(':')[1].slice(1, (fh[i].split(',')[7]).split(':')[1].length - 1).replace('\\', '') + ' (' + classRows[fh[i].split(',')[1].split(':')[1]].replace('"', '').replace('"', '') + ', due ' + fh[i].split(',')[5].split(':')[1].replace('\\', '').slice(1, fh[i].split(',')[5].split(':')[1].replace('\\', '').length - 1) + ')';
+        if ((fh[i].split(',')[3]).split(':')[1] == '"assignment-upcoming"'){
+            var dueDate = fh[i].split(',')[5].split(':')[1].replace('\\', '').slice(1, fh[i].split(',')[5].split(':')[1].replace('\\', '').length - 1);
+            var dueDatelis = [monthdict[dueDate.slice(0, 3)], parseInt(dueDate.slice(4, dueDate.length))];
+            var assignStr = '';
+            let commaMuch = fh[i].split(',').length - 11;
+            if (commaMuch <= 0){
+                assignStr =  'Due ' +  dueDate + ' - ' + (fh[i].split(',')[7]).split(':')[1].slice(1, (fh[i].split(',')[7]).split(':')[1].length - 1).replace('\\', '') + ' (' + classRows[fh[i].split(',')[1].split(':')[1]].replace('"', '').replace('"', '') + ')';
             }
-            else{ // For assignments with commas
+            else{
                 for (let o = 0; o <= commaMuch; o++){
                     if (o==0){
-                        assignStr = assignStr + fh[i].split(',')[7].split(':')[1]; // Only this part has a colon
+                        assignStr = assignStr + fh[i].split(',')[7].split(':')[1];
                     }
                     else{
                         assignStr = assignStr + ',';
                         assignStr = assignStr + fh[i].split(',')[7 + o];
                     }
                 }
-		// Some final operations to clean up the string
-                assignStr = assignStr.slice(1, assignStr.length - 1).replace('\\', '') + ' (' + classRows[fh[i].split(',')[1].split(':')[1]].replace('"', '').replace('"', '') + ', due ' + fh[i].split(',')[5].split(':')[1].replace('\\', '').slice(1, fh[i].split(',')[5].split(':')[1].replace('\\', '').length - 1) + ')';
+                assignStr = 'Due ' + dueDate  + ' - ' + assignStr.slice(1, assignStr.length - 1).replace('\\', '') + ' (' + classRows[fh[i].split(',')[1].split(':')[1]].replace('"', '').replace('"', '') + ')';
             }
-	    for (let j = 0; j < assignments.length; j++){ //Code to make sure no assignment repeats
+            if(dueDatelis[0] < today.getMonth() + 1){ // Automatically removes less month
+                isOn = true;
+            }
+            else{
+                if(dueDatelis[1] < today.getDate()){
+                    isOn = true;
+                }
+                else if(dueDatelis[1] == today.getDate()  && dueDatelis[0] == today.getMonth() + 1){ // If the assignment is due the day of
+                    assignStr = 'Due today - ' + assignStr.slice(dueDate.length + 7, assignStr.length);
+                    isToday = true;
+
+                }
+                else if(dueDatelis[1] == tommorow.getDate() && dueDatelis[0] == tommorow.getMonth() + 1){ // If the assignment is due the day after
+                    assignStr = 'Due tommorow - ' + assignStr.slice(dueDate.length + 7, assignStr.length);
+                    isTmrw = true;
+                }
+            }
+	    for (let j = 0; j < assignments.length; j++){ // For assignments due later than a day after the date the program is runned in
                 if(assignments[j] == assignStr){
                     isOn = true;
-	        }
+	            }
             }
-            if(!isOn){ 
-                assignments.push(assignStr);
+            if(!(assignDate['today'] == null)){ // If is an assignment due today that may be repeated
+                for (let j = 0; j < assignDate['today'].length; j++){
+                    if(assignDate['today'][j] == assignStr){
+                         isOn = true;
+                    }
+                }
             }
-	    isOn = false; // Resets for every iteration
+	    if(!(assignDate['tmrw'] == null)){ // If an assignment that is due tommorow is repeated
+                for (let j = 0; j < assignDate['tmrw'].length; j++){
+		    if(assignDate['tmrw'][j] == assignStr){
+		        isOn = true;
+		    }
+		}
+	    }
+            if(!isOn){ //The assignment should only appear once
+                if(!isToday){
+                    if(isTmrw){
+                        if(assignDate['tmrw'] == null){
+                            assignDate['tmrw'] = [assignStr];
+                        }
+                        else{
+                            assignDate['tmrw'].push(assignStr);
+                        }
+                    }
+                    else{
+                        assignments.push(assignStr);
+                    }
+                }
+                else{
+                    if (assignDate['today'] == null){
+                        assignDate['today'] = [assignStr];
+                    }
+                    else{
+                        assignDate['today'].push(assignStr);
+                    }
+                }
+            }
+	    isOn = false;
         }
     }
-    var g = document.getElementById('assign'); // Actually adding the assignments to the empty div in popup.html
+    var g = document.getElementById('assign'); // Getting the empty div in popup.html
     var pinit = document.createElement('p');
+    pinit.style = "font-size: 150%; text-align: center;";
     pinit.appendChild(document.createTextNode('Here are your upcoming assignments:'));
     g.appendChild(pinit);
-    for (let k = 0; k < assignments.length; k++){ // Assignment adding
+    for (let k = 0; k < assignDate['today'].length; k++){ // Assignments due today
         var h = document.createElement('p');
-	h.appendChild(document.createTextNode(assignments[k]));
-	g.appendChild(h);
+        h.appendChild(document.createTextNode(assignDate['today'][k]));
+        g.appendChild(h);
     }
-    var pf = document.createElement('p') // Adding link to upcoming assignments at the end
+    for (let k = 0; k < assignDate['tmrw'].length; k++){ // Assignments due tommoroow
+        var h = document.createElement('p');
+        h.appendChild(document.createTextNode(assignDate['tmrw'][k]));
+        g.appendChild(h);
+    }
+    var pmed = document.createElement('p');
+    pmed.appendChild(document.createTextNode('Due later (sorted by class):'))
+    pmed.style = "font-size: 150%; text-align: center;";
+    g.appendChild(pmed);
+    for (let k = 0; k < assignments.length; k++){
+        var h = document.createElement('p');
+	    h.appendChild(document.createTextNode(assignments[k]));
+	    g.appendChild(h);
+    }
+    var pf = document.createElement('p')
     pf.appendChild(document.createTextNode('If you want more info, visit '));
     var a = document.createElement('a');
     a.href = "https://portals.veracross.com/webb/student/student/upcoming-assignments";
     a.appendChild(document.createTextNode('your upcoming assignments page'));
     pf.appendChild(a);
+    pf.style = "text-align: center;"
     pf.appendChild(document.createTextNode('!'));
     g.appendChild(pf);
 }
 
 
 function reqNotify(){
-    if (!(assign)){ // For only being able to see it once to prevent massivley long popup page
+    if (!(assign)){
 	getAssignments();
         assign = true;
     }
 }
-
