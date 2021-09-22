6b5c012b0c8b8e6027c108670af0895559023eef
Dmitri K <69704069+DtPeach3707@users.noreply.github.com>
2021-09-22 07:52:38 -0400

    Improve now improves more

--- background.js
+++ background.js
@@ -48,7 +48,28 @@ function improve(type){
                 li.appendChild(document.createElement('br')); // Line breaks (to make it look nicer)
                 li.appendChild(document.createElement('br'));
                 nedrop.appendChild(li);      
-            }    
+            }
+	case 2: // Takes class ID out of every class for Dropbox. Eventually it will also go directly to the bottom. 
+            var si = document.getElementsByClassName('full-screen-bg')[0].getElementsByClassName('app-container')[0];
+            var g = si.getElementsByTagName('div')[0].getElementsByClassName('assignment-submission')[0].getElementsByClassName('ae-grid')[0].getElementsByClassName('item-xs-12')[1].getElementsByClassName('vx-record-detail');
+            var conf = document.getElementById('confirm-div');
+            if (conf==null){
+                for (k = 0; k < g.length; k ++){
+                var h = g[k].getElementsByClassName('vx-record-header')[0].getElementsByClassName('vx-record-header__title')[0];
+                if (h.innerHTML.split(':').length > 2){
+                    finalclassName = h.innerHTML.split(':')[1]
+                    for (l = 0; l < h.innerHTML.split(':').length - 2; l++){
+                        finalclassName = finalclassName + ': ' + h.innerHTML.split(':')[2+l];
+                    }
+                    h.innerHTML = finalclassName;            }
+                else{
+                    h.innerHTML = h.innerHTML.split(':')[1];
+            }
+         }
+         var confdiv = document.createElement('div');
+         confdiv.id = 'confirm-div';
+         si.appendChild(confdiv)
+      }
 		   
     }
 
@@ -64,6 +85,9 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	if (tab.url.match(/assignment/) && tab.url.match(/detail/)){ // Specific part of the domain improve is improving
 	    chrome.scripting.executeScript({target: {tabId: tabId}, func: improve, args: [1]}); // For assignment detail
 	}
+	else if (tab.url.match(/submit-assignments/)){ // Dropbox improvement (more features are to be added!)
+            chrome.scripting.executeScript({target: {tabId: tabId}, func: improve, args: [2]});
+        }
 	// Other parts if portals.veracross.com improvement in the works!
     }
 });
