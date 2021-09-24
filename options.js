2c34f13c829b0ecd3851192fc5ed69c328270024
NicholasMirochnikoff <90470471+NicholasMirochnikoff@users.noreply.github.com>
2021-09-24 11:53:55 -0400

    Update options.js
    Just found this section a little confusing thinking that it could be shortened from 37 to 23 lines of code I commented out the code before though so you can compare.

--- options.js
+++ options.js
@@ -1,6 +1,6 @@
 //options.js
 //For light dark mode on options page
-
+/*
 var lightmode = true;
 var r = document.querySelector(':root');
 
@@ -38,3 +38,30 @@ function changeMode(){
 	lightMode();
     }
 }
+*/
+//Top is before bottom is proposed changes
+
+var lightmode = true;
+var r = document.querySelector(':root');
+
+document.getElementById('changeColor').addEventListener('click', changeMode);
+
+function changeMode(){
+	if (lightmode){
+		changeColor.innerHTML = "Press button for light mode";
+		lightmode = false;
+		r.style.setProperty('--background-color', '#000000');
+		r.style.setProperty('--border-color', '#ffffff');
+	}
+	else{
+		changeColor.innerHTML = "Press button for dark mode";
+		lightmode = true;
+		r.style.setProperty('--background-color', '#ffffff');
+		r.style.setProperty('--border-color', '#000000');
+	}
+	chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+	var activeTab = tabs[0];
+	chrome.tabs.sendMessage(activeTab.id, {"message": "email"});
+	});
+}
+
