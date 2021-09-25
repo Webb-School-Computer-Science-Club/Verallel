8b840f34d8bfa007b16ee1e9a3d71d384f17f173
Dmitri K <69704069+DtPeach3707@users.noreply.github.com>
2021-09-25 08:29:12 -0400

    Deleted commented before code and changed message
    Email was originally for assignment notifications. Since this is done via fetching assignments, email is no longer necessary, only changing mode is.

--- options.js
+++ options.js
@@ -1,45 +1,6 @@
 //options.js
 //For light dark mode on options page
-/*
-var lightmode = true;
-var r = document.querySelector(':root');
 
-document.getElementById('changeColor').addEventListener('click', changeMode);
-
-function darkMode(){
-    r.style.setProperty('--background-color', '#000000');
-    r.style.setProperty('--border-color', '#ffffff');
-    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {"message": "email"});
-    });
-}
-
-
-function lightMode(){
-    r.style.setProperty('--background-color', '#ffffff');
-    r.style.setProperty('--border-color', '#000000');
-    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {"message": "email"});
-    });
-}
-
-
-function changeMode(){
-    if (lightmode == true){
-	changeColor.innerHTML = "Press button for light mode";
-	lightmode = false;
-	darkMode();
-    }
-    else{
-        changeColor.innerHTML = "Press button for dark mode";
-	lightmode = true;
-	lightMode();
-    }
-}
-*/
-//Top is before bottom is proposed changes
 
 var lightmode = true;
 var r = document.querySelector(':root');
@@ -61,7 +22,7 @@ function changeMode(){
 	}
 	chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
 	var activeTab = tabs[0];
-	chrome.tabs.sendMessage(activeTab.id, {"message": "email"});
+	chrome.tabs.sendMessage(activeTab.id, {"message": "change"});
 	});
 }
 
