21296183fb94d0a15e1f18dfc11824247b45aa0a
NicholasMirochnikoff <90470471+NicholasMirochnikoff@users.noreply.github.com>
2021-09-25 17:17:53 -0400

    Option.js Update#2
    More nitty gritty reorganization

--- options.js
+++ options.js
@@ -7,22 +7,27 @@ var r = document.querySelector(':root');
 
 document.getElementById('changeColor').addEventListener('click', changeMode);
 
-function changeMode(){
-	if (lightmode){
+function changeMode()
+{
+	if (lightmode)
+	{
 		changeColor.innerHTML = "Press button for light mode";
 		lightmode = false;
 		r.style.setProperty('--background-color', '#000000');
 		r.style.setProperty('--border-color', '#ffffff');
 	}
-	else{
+	else
+	{
 		changeColor.innerHTML = "Press button for dark mode";
 		lightmode = true;
 		r.style.setProperty('--background-color', '#ffffff');
 		r.style.setProperty('--border-color', '#000000');
 	}
-	chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
-	var activeTab = tabs[0];
-	chrome.tabs.sendMessage(activeTab.id, {"message": "change"});
+	
+	chrome.tabs.query({currentWindow: true, active: true}, function (tabs)
+	{
+		var activeTab = tabs[0];
+		chrome.tabs.sendMessage(activeTab.id, {"message": "change"});
 	});
 }
 
