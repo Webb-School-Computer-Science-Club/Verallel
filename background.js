e08efdce539996070a20af5c49b2e085db1fb72f
Dmitri K <69704069+DtPeach3707@users.noreply.github.com>
2021-09-25 18:19:10 -0400

    Additional restyling

--- background.js
+++ background.js
@@ -4,13 +4,13 @@
 
 function improve(type)
 {
-	switch(type)
-	{ // Uses switch because more types will be added in the future for different class tabs
+	switch(type) // Uses switch because more types will be added in the future for different class tabs
+	{ 
 			
 		case 0:
 			var lin = document.getElementById('better-link-class-home');
-			if (lin == null)
-			{ //Link shouldn't appear twice
+			if (lin == null) //Link shouldn't appear twice
+			{ 
 				var h = ((document.getElementById('container').getElementsByClassName('class-header')[0]).getElementsByTagName('h1')[0]).getElementsByTagName('a')[0];
 				h.innerHTML = (h.innerHTML).split(':')[1]; // Removing mess of numbers (probably class ID) from class page to make it look nicer
 				var needrop = document.getElementById('facebox').getElementsByClassName('popup')[0]; // Element exists even when not actually visible
@@ -31,7 +31,7 @@ function improve(type)
 
 
 		case 1: // assignments detail page
-			var lin = document.getElementById('link-to-dropbox');
+		        var lin = document.getElementById('link-to-dropbox');
 			if(lin == null)
 			{
 				var h = document.getElementsByClassName('full-screen-bg')[0].getElementsByClassName('app-container')[0].getElementsByClassName('detail-assignment')[0].getElementsByClassName('vx-record-header')[0].getElementsByClassName('ae-grid')[0].getElementsByClassName('ae-grid__item')[0].getElementsByClassName('vx-record-header__title')[0];
@@ -56,32 +56,32 @@ function improve(type)
 
 
 		case 2: // Takes class ID out of every class for Dropbox. Eventually it will also go directly to the bottom. 
-		var si = document.getElementsByClassName('full-screen-bg')[0].getElementsByClassName('app-container')[0];
-		var g = si.getElementsByTagName('div')[0].getElementsByClassName('assignment-submission')[0].getElementsByClassName('ae-grid')[0].getElementsByClassName('item-xs-12')[1].getElementsByClassName('vx-record-detail');
-		var conf = document.getElementById('confirm-div');
-		if (conf==null)
-		{
-			for (k = 0; k < g.length; k ++)
+			var si = document.getElementsByClassName('full-screen-bg')[0].getElementsByClassName('app-container')[0];
+			var g = si.getElementsByTagName('div')[0].getElementsByClassName('assignment-submission')[0].getElementsByClassName('ae-grid')[0].getElementsByClassName('item-xs-12')[1].getElementsByClassName('vx-record-detail');
+			var conf = document.getElementById('confirm-div');
+			if (conf==null)
 			{
-				var h = g[k].getElementsByClassName('vx-record-header')[0].getElementsByClassName('vx-record-header__title')[0];
-				if (h.innerHTML.split(':').length > 2)
+				for (k = 0; k < g.length; k ++)
 				{
-					finalclassName = h.innerHTML.split(':')[1]
-					for (l = 0; l < h.innerHTML.split(':').length - 2; l++)
+					var h = g[k].getElementsByClassName('vx-record-header')[0].getElementsByClassName('vx-record-header__title')[0];
+					if (h.innerHTML.split(':').length > 2)
 					{
-						finalclassName = finalclassName + ': ' + h.innerHTML.split(':')[2+l];
+						var finalclassName = h.innerHTML.split(':')[1]
+						for (l = 0; l < h.innerHTML.split(':').length - 2; l++)
+						{
+							finalclassName = finalclassName + ': ' + h.innerHTML.split(':')[2+l];
+						}
+						h.innerHTML = finalclassName;
 					}
-					h.innerHTML = finalclassName;
+					else
+					{
+						h.innerHTML = h.innerHTML.split(':')[1];
+					}
 				}
-				else
-				{
-					h.innerHTML = h.innerHTML.split(':')[1];
-				}
+				var confdiv = document.createElement('div');
+				confdiv.id = 'confirm-div';
+				si.appendChild(confdiv)
 			}
-			var confdiv = document.createElement('div');
-			confdiv.id = 'confirm-div';
-			si.appendChild(confdiv)
-		}
 	}
 }
 
@@ -94,13 +94,14 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab)
 	}
 	else if (tab.url.match(/portals.veracross.com/) && tab.url.match(/student/))
 	{
-		if (tab.url.match(/assignment/) && tab.url.match(/detail/))
-		{ // Specific part of the domain improve is improving
+		if (tab.url.match(/assignment/) && tab.url.match(/detail/)) // Specific part of the domain improve is improving
+		{ 
 			chrome.scripting.executeScript({target: {tabId: tabId}, func: improve, args: [1]}); // For assignment detail
 		}
-		else if (tab.url.match(/submit-assignments/))
-		{ // Dropbox improvement (more features are to be added!)
+		else if (tab.url.match(/submit-assignments/)) // Dropbox improvement (more features are to be added here!)
+		{ 
 			chrome.scripting.executeScript({target: {tabId: tabId}, func: improve, args: [2]});
 		}
-	}// Other parts if portals.veracross.com improvement in the works!
+	} 
+	// Other parts if portals.veracross.com improvement in the works!
 });
